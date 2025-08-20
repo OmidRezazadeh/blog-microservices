7a80129305506015f618bb29484a25c5ccc129c0
orprogrammer <orprogrammer@gmail.com>
2025-08-21 02:31:19 +0330

    Remove unused user service and authentication files to streamline project structure
    - Deleted `Login.dto.ts`, `user.entity.ts`, and `auth.module.ts` to eliminate redundant code and improve maintainability.
    - This cleanup supports ongoing refactoring efforts and prepares for future enhancements.

--- apps/user-service/src/dto/Login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty  } from "class-validator";
-export class LoginDto{
-    @IsNotEmpty({ message: 'ایمیل نمی‌تواند خالی باشد' })
-    @IsEmail({}, { message: 'ایمیل وارد شده صحیح نیست ' })
-    email:string
-    
-    @IsNotEmpty()
-    password:string
-}
