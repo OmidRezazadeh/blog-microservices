e14f3fb520c2a339e8b1d1db29fc5daa7bd4df6e
orprogrammer <orprogrammer@gmail.com>
2025-08-23 17:34:32 +0330

    Enhance API Gateway with Post Module Integration
    - Added PostModule, PostController, and PostService to the API Gateway for post management functionality.
    - Updated imports and providers in ApiGatewayModule to include the new post-related components.
    - This enhancement allows for the creation and management of posts within the API Gateway.

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -4,10 +4,13 @@ import { ApiGatewayService } from './api-gateway.service';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { UsersModule } from './user/user.module';
+import { PostService } from './post/post.service';
+import { PostController } from './post/post.controller';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [ApiGatewayController, UserController],
-  providers: [ApiGatewayService, UserService],
+  imports: [UsersModule, PostModule],
+  controllers: [ApiGatewayController, UserController, PostController],
+  providers: [ApiGatewayService, UserService, PostService],
 })
 export class ApiGatewayModule {}
